720dbd2f2cea0303df16ba4c806feddebe23dde7
canbo <canbo@HPVICTUS>
2023-12-15 19:28:59 +0300

    Proje dosyası ekle.

---
+++ ders/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad=textBox1.Text;
+            string soyad=textBox2.Text;
+            string meslekk = textBox4.Text;
+            int yaş=Convert.ToInt32(textBox3.Text);
+            Meslek meslek = new Meslek();
+            meslek.isim(ad, soyad, meslekk, yaş);
+        }
+    }
+}
---
+++ ders/Meslek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders
+{
+    internal class Meslek
+    {
+        public void isim(string ad,string soyad,string meslek,int yaş)
+        {
+            MessageBox.Show("İsim: " + ad + " Soyisim: " + soyad + " Meslek: " + meslek+" Yaş: "+yaş);
+        }
+        
+    }
+}
